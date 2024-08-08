2320a5f7bf9c2894a9c61bd5e955853957a9d8c0
Samarth <samarthambure67@gmail.com>
2024-08-08 11:43:08 +0530

    Created popup to Add Restaurant

--- foodie-delight/src/pages/AddRestaurant.js
+++ foodie-delight/src/pages/AddRestaurant.js
@@ -1,12 +1,40 @@
-import React from 'react'
-import Layout from '../components/Layout/Layout'
+import React from "react";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import { Button, DialogContent } from "@mui/material";
+import "../styles/AddRestaurant.css";
+import CloseIcon from "@mui/icons-material/Close";
+import {Typography } from "@mui/material";
 
-const AddRestaurant = () => {
+export default function AddRestaurant(props) {
+  const { title, children, openPopup, setOpenPopup } = props;
+
   return (
-    <Layout>
-        <h1>Add Restaurant Page</h1>
-    </Layout>
-  )
+    <div>
+      <Dialog
+        open={openPopup}
+        maxWidth="md"
+        classes={{ paper: "dialogWrapper" }}
+      >
+        <DialogTitle>
+          <div style={{ display: "flex" }}>
+            <Typography variant="h6" component="div" style={{ flexGrow: 1 }}>
+              {title}
+            </Typography>
+            <Button
+              color= "secondary"
+              onClick={() => {
+                setOpenPopup(false);
+              }}
+            >
+              <CloseIcon />
+            </Button>
+          </div>
+        </DialogTitle>
+        <DialogContent>
+          <div>Content goes Here</div>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
 }
-
-export default AddRestaurant
--- foodie-delight/src/pages/ListRestaurants.js
+++ foodie-delight/src/pages/ListRestaurants.js
@@ -9,40 +9,57 @@ import {
   CardMedia,
   Typography,
 } from "@mui/material";
-import { Link } from "react-router-dom";
-
+import AddRestaurant from "../pages/AddRestaurant";
+import { useState } from "react";
 const Menu = () => {
+  const [openPopup, setopenPopup] = useState(false);
+  function openPopupNow() {
+    setopenPopup(true);
+  }
   return (
-    <Layout>
-      <div className="headerContainer">
-        <Link to="/addRestaurant" className="button-link">
-          <button>Add Restaurant</button>
-        </Link>
-      </div>
-      <Box sx={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
-        {restaurantList.map((menu) => (
-          <Card sx={{ maxWidth: "390px", display: "flex", m: 2 }}>
-            <CardActionArea>
-              <CardMedia
-                sx={{ minHeight: "400px" }}
-                component={"img"}
-                src={menu.image}
-                alt={menu.name}
-              />
-              <CardContent>
-                <Typography variant="h5" gutterBottom component={"div"}>
-                  {menu.name}  
-                </Typography>
-                <Typography variant="body2">Location: {menu.location}</Typography>
-                <Typography variant="body2">Price for 2: {menu.price}</Typography>
-                <Typography variant="body2">Discount: {menu.discount}</Typography>
-                <Typography variant="body2">{menu.description}</Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        ))}
-      </Box>
-    </Layout>
+    <>
+      <Layout>
+        <div className="headerContainer">
+          <button onClick={openPopupNow}>Add Restaurant</button>
+        </div>
+        <Box
+          sx={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}
+        >
+          {restaurantList.map((menu) => (
+            <Card sx={{ maxWidth: "390px", display: "flex", m: 2 }}>
+              <CardActionArea>
+                <CardMedia
+                  sx={{ minHeight: "400px" }}
+                  component={"img"}
+                  src={menu.image}
+                  alt={menu.name}
+                />
+                <CardContent>
+                  <Typography variant="h5" gutterBottom component={"div"}>
+                    {menu.name}
+                  </Typography>
+                  <Typography variant="body2">
+                    Location: {menu.location}
+                  </Typography>
+                  <Typography variant="body2">
+                    Price for 2: {menu.price}
+                  </Typography>
+                  <Typography variant="body2">
+                    Discount: {menu.discount}
+                  </Typography>
+                  <Typography variant="body2">{menu.description}</Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          ))}
+        </Box>
+      </Layout>
+      <AddRestaurant
+        title="Add Restaurant"
+        openPopup={openPopup}
+        setOpenPopup={setopenPopup}
+      ></AddRestaurant>
+    </>
   );
 };
 
